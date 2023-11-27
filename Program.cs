ec53235a63bd47a84d9d2271ab9be35e077578c5
ajlunagrupocie <ajluna@grupocie.com>
2023-11-27 09:24:27 +0100

    a

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CieIntercalaImpresion
 {
@@ -21,31 +22,65 @@ namespace CieIntercalaImpresion
         static string modoPDF;
         static string usuarioSage;
         static string contUsuario;
+        static string soloImprimir;
 
         static string rutaFicDest;
-        static string rutaFicDestPDF;
+        static string rutaFichDFDestPDF;
 
         static void Main(string[] args)
         {
             /* ASIGNAMOS PARÁMETROS A VARIABLES */
             //args[0] : Ruta fichero
 
+            if (args.Length <= 1)
+            {
+                string instrucciones;
+                instrucciones = "Este programa permite añadir un documento pdf a una cola de impresión. Parámetros(Todos son de caracter obligatorio):";
+                instrucciones += '\r' + "args[0] : Rutafichero. Ruta completa del fichero origen.";
+                instrucciones += '\r' + "args[1] : Nombrefichero. Denominación fichero, acabada en .pdf";
+                instrucciones += '\r' + "args[2] : Impresora. Impresora destino.";
+                instrucciones += '\r' + "args[3] : ModoPdf. N -> modo desactivado. S -> modo activado, no saldrá nada por la impresora y los pdf se generaran en un directorio con el formato PDF_UsuarioSage_contadorUsuario_";
+                instrucciones += '\r' + "args[4] : UsuarioSage. Código Usuario Sage";
+                instrucciones += '\r' + "args[5] : contadorUsuario. Este parámetro se utiliza para controlar que solo haya una carpeta PDF por usuario y contador, además todas las impresiones que se envien con un mismo contador se guardarán en una misma carpeta.";
+                instrucciones += '\r' + "args[6] : rutaFichDFDestPDF. Por defecto -. En este caso la ruta será " + @"c:\GRUPOCIE";
+                instrucciones += '\r' + "args[7] : soloImprimir. S -> Solo inserta el documento en la cola de la impresora instanciada, no intercala. N -> opción desactiva (por defecto)";
 
+                MessageBox.Show(instrucciones, "Instrucciones");
+                Environment.Exit(0);
+            }
             /*
-            rutaFicheroOrigen = args[0];
-            //args[1] : Nombre fichero
-            nombreFicheroOrigen = args[1];
-            //args[2] : Impresora
-            impresora = args[2];
-            //args[3] : ModoPdf
-            modoPDF = args[3];
-            //args[4] : UsuarioSage
-            usuarioSage = args[4];
-            //args[5] : contadorUsuario
-            contUsuario = args[5];
-            
+            foreach (string x in args)
+            {
+                MessageBox.Show(x, "Debug");
+            }
+            Environment.Exit(0);
             */
 
+            try
+            {
+                //args[0] : Ruta fichero origen
+                rutaFicheroOrigen = args[0];
+                //args[1] : Nombre fichero
+                nombreFicheroOrigen = args[1];
+                //args[2] : Impresora
+                impresora = args[2];
+                //args[3] : ModoPdf
+                modoPDF = args[3];
+                //args[4] : UsuarioSage
+                usuarioSage = args[4];
+                //args[5] : contadorUsuario
+                contUsuario = args[5];
+                //args[7] : rutaFichDFDestPDF
+                rutaFichDFDestPDF = args[6];
+                //args[8] : soloImprimir
+                soloImprimir = args[7];
+            }
+            catch (Exception ex)
+            {
+                new EscribirLog("Error de inicialización de argumentos: " + ex.Message, false);
+                Environment.Exit(0);
+
+            }
 
 
             //string[] argumentos = args[1].Split(';');
@@ -53,6 +88,7 @@ namespace CieIntercalaImpresion
             //nombreFicheroOrigen = argumentos[1];
             //impresora = argumentos[2];
 
+            /*
 
             rutaFicheroOrigen = @"C:\GRUPOCIE\PRUEBAANDREU2.pdf";
              nombreFicheroOrigen = "PRUEBAANDREU2.pdf";
@@ -60,11 +96,32 @@ namespace CieIntercalaImpresion
             
              impresora = "OneNote for Windows 10";
 
+            rutaFichDFDestPDF = "-";
+
             modoPDF = "S";
             usuarioSage = "1";
             contUsuario = "3";
 
+           */
 
+            if (soloImprimir == "S")
+            {
+                try
+                {
+                    rutaFicheroDestino = rutaFicheroOrigen;
+                    imprimirFichero();
+                }
+                catch (Exception ex)
+                {
+                    new EscribirLog("Error al imprimir el fichero: " + ex.Message, false);
+
+                }
+                finally
+                {
+                    //EscribirLog.eliminarFichero(5);
+                    Environment.Exit(0);
+                }
+            }
 
 
             try
@@ -228,16 +285,33 @@ namespace CieIntercalaImpresion
             }
             else 
             {
+                string rutaFicDestPDF;
+              //  string rutaFicDestPDF = "";
+                string rufaficPDFConfig = rutaFichDFDestPDF;
+                if (rufaficPDFConfig == "-")
+                {
+                    rufaficPDFConfig = @"C:\GRUPOCIE";
+                }
+                // copiamos el fichero a la carpeta nueva
+                if (!Directory.Exists(rufaficPDFConfig))
+                {
+                    Directory.CreateDirectory(rufaficPDFConfig);
+                }
+                rufaficPDFConfig += @"\PDFs";
+                if (!Directory.Exists(rufaficPDFConfig))
+                {
+                    Directory.CreateDirectory(rufaficPDFConfig);
+                }
                 // SI NO EXISTE EL DIRECTORIO DEL USUARIO Y EL CONTADOR, LO CREAMOS
-                rutaFicDestPDF = rutaFicDest + @"\PDFs_User-" + usuarioSage + "_" + contUsuario + "_";
+                rutaFicDestPDF = rufaficPDFConfig + @"\PDFs_User-" + usuarioSage + "_" + contUsuario + "_";
                 if (!Directory.Exists(rutaFicDestPDF))
                 {
                     Directory.CreateDirectory(rutaFicDestPDF);
                 }
 
                 //ELIMINAMOS CARPETAS ANTIGUAS SI EXISTIERAN
-                string[] dirs = Directory.GetDirectories(rutaFicDest);
-
+                string[] dirs = Directory.GetDirectories(rufaficPDFConfig);
+                
                 foreach (string dir in dirs)
                 {
                     if ((dir.Contains(@"\PDFs_User-" + usuarioSage + "_")) && !(dir.Contains(@"\PDFs_User-" + usuarioSage + "_" + contUsuario + "_")))
@@ -245,8 +319,9 @@ namespace CieIntercalaImpresion
                         Directory.Delete(dir, true);
                     }
                 }
-                // copiamos el fichero a la carpeta nueva
-                File.Copy(rutaFicheroDestino, rutaFicDestPDF+@"\"+ nombreFicheroDestino);
+                
+
+                File.Copy(rutaFicheroDestino, rutaFicDestPDF + @"\"+ nombreFicheroDestino);
                 
 
             }
